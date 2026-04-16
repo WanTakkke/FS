daa63eb92b89a5c9837492d8930b2e20b96da383
wantakkke <1018902416@qq.com>
2026-04-16 16:12:17 +0800

    feat: 解决跨域+新增student映射类

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from controller import studentController
 
 app = FastAPI()
+
+# 配置CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有HTTP地址访问
+    allow_credentials=True,  # 允许发送凭据（cookies、认证头部等）
+    allow_methods=["*"],  # 允许所有HTTP方法
+    # 或指定方法：allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    allow_headers=["*"],  # 允许所有HTTP头部
+    # 或指定头部：allow_headers=["Authorization", "Content-Type"]
+)
 
 #导入子路由
 app.include_router(studentController.stu_router)
--- models/studentInfo.py
+++ models/studentInfo.py
@@ -1,10 +1,23 @@
-from sqlalchemy import String
+from datetime import date
+from sqlalchemy import String, Integer, Date
+from sqlalchemy.dialects.mysql import TINYINT
 from sqlalchemy.orm import Mapped, mapped_column
+from models import baseInfo
 
 
-class StudentInfo():
+class StudentInfo(baseInfo):
     __tablename__ = "students"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, comment="自增主键")
-    student_code: Mapped[str] = mapped_column(String(50), comment="学号")
-    class_id: Mapped[int] = mapped_column(comment="班级ID")
+    student_code: Mapped[str] = mapped_column(String(50), nullable=False, unique=True, comment="学生业务编号")
+    class_id: Mapped[int] = mapped_column(Integer, nullable=False, comment="所属班级ID (逻辑关联 classes.id)")
+    advisor_id: Mapped[int] = mapped_column(Integer, comment="顾问老师ID (逻辑关联 teachers.id)")
+    name: Mapped[str] = mapped_column(String(50), nullable=False, comment="姓名")
+    gender: Mapped[int] = mapped_column(TINYINT, nullable=False, comment="性别：1-男，2-女")
+    age: Mapped[int] = mapped_column(Integer, comment="年龄")
+    hometown: Mapped[str] = mapped_column(String(100), comment="籍贯")
+    graduate_school: Mapped[str] = mapped_column(String(100), nullable=False, comment="毕业院校")
+    major: Mapped[str] = mapped_column(String(100), comment="专业")
+    enrollment_date: Mapped[date] = mapped_column(Date, comment="入学时间")
+    graduation_date: Mapped[date | None] = mapped_column(Date, comment="毕业时间")
+    education_level: Mapped[str | None] = mapped_column(String(50), comment="学历")
